af8f38bb3cfe91cd6913b3244459926bc4d874e2
Ernesto Lechuga <bicerya2023@gmail.com>
2023-03-17 16:47:17 -0600

    curso basico de Js

---
+++ cursoFront/cursoJavaScript/valores.js
@@ -0,0 +1,11 @@
+Number = 40;
+
+String = "Ernesto Lechuga"
+
+Boolean = true / false
+
+null / undefined
+
+Array = ["valores primitivos"]
+
+Object = {nombre:"Ernesto",apellido:"Garcia"}
---
+++ cursoFront/cursoJavaScript/variables.js
@@ -0,0 +1,18 @@
+//declarar e inicializaer variables
+let nombre = "Ernesto"
+
+//declarar variable vacia
+let edad;
+
+//inicializar la variable
+edad = 30;
+
+let elementos = ["Computadora","Celular","Pantalla"];
+
+let persona = {
+    nombre: "Ernesto",
+    edad: 30,
+
+}
+
+persona;
